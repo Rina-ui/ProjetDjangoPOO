0b42818738fe573cf0d800c37af7d54f820f4e22
Rina-ui <marinagracia895@gmail.com>
2026-03-17 21:45:46 +0000

    mise en place de l'investment pour le dashboard du owner

--- front/src/kora-dashboard/three.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="three" />
-
-declare global {
-    // THREE est chargé via CDN dans index.html (three.min.js r128)
-    // eslint-disable-next-line no-var
-    var THREE: typeof import("three")
-}
-
-export {}
